78ee3748cef66c0e0c12977d828f79e59662be01
Pranay Lad <142432489+Pranay-Lad@users.noreply.github.com>
2024-01-06 11:20:19 +0000

    Update keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,9 +2,9 @@ local keymap = vim.keymap
 
 vim.g.mapleader = " "
 
-keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
-keymap.set("n", "<leader>l", ":Neotree focus<CR>")
+keymap.set("n", "<leader>e", "<Cmd>Neotree toggle<CR>")
+keymap.set("n", "<leader>l", "<Cmd>Neotree focus<CR>")
 
-keymap.set("n", "<leader>r", ":VimtexCompile<CR>")
-keymap.set("n", "<leader>cc", ":VimtexStop<CR> :VimtexClean<CR>")
+keymap.set("n", "<leader>r", "<Cmd>VimtexCompile<CR>")
+keymap.set("n", "<leader>cc", "<Cmd>VimtexStop<CR> <Cmd>VimtexClean<CR>")
 
